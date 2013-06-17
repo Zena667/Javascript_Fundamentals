d0f0b24029699fb71c229d55bf38df194e64730f
Zena B <zena667@gmail.com>
2013-06-17 13:31:38 -0400

    fortuneTeller fix missing +, AgeCalculator Exercise 2 finish

--- fortuneTeller.js
+++ fortuneTeller.js
@@ -2,5 +2,5 @@ var numChildren = 3;
 var partnerName = 'Natasha';
 var location = 'Toronto';
 var jobTitle = 'Queen';
-var future = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' partnerName + ' with ' + numChildren '.';
+var future = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' + partnerName + ' with ' + numChildren + ' children.';
 console.log(future);
---
+++ theAgeCalculator.js
@@ -0,0 +1,4 @@
+var currentYear = 2013;
+var birthYear = 1976;
+var age = (currentYear - birthYear);
+console.log('You are either ' + age + ' or ' + (age - 1) + '.');
