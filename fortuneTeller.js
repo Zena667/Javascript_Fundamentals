f7956fee6f77a79660d3bbf918d90c55890e7b8a
Zena B <zena667@gmail.com>
2013-06-17 13:24:43 -0400

    fortune teller ex. 1

---
+++ fortuneTeller.js
@@ -0,0 +1,6 @@
+var numChildren = 3;
+var partnerName = 'Natasha';
+var location = 'Toronto';
+var jobTitle = 'Queen';
+var future = 'You will be a ' + jobTitle + ' in ' + location + ' and married to ' partnerName + ' with ' + numChildren '.';
+console.log(future);
